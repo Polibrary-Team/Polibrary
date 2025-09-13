e57f6868ebc9446cfe635a9de4de64b8328e058c
Fapingvin123 <fapingvin@gmail.com>
2025-09-13 19:43:29 +0200

    version get ready for emergency fix

--- src/Main.cs
+++ src/Main.cs
@@ -40,7 +40,7 @@ public static class Main
         new Harmony("com.polibraryteam.polibrary").PatchAll();
         modLogger = logger;
         logger.LogMessage("Polibrary.dll loaded.");
-        modLogger.LogMessage("Version 2.1");
+        modLogger.LogMessage("Version 2.1-pre");
         PolyMod.Loader.AddPatchDataType("cityRewardData", typeof(CityReward)); //casual fapingvin carry
         PolyMod.Loader.AddPatchDataType("unitEffectData", typeof(UnitEffect)); //casual fapingvin carry... ...again
         PolyMod.Loader.AddPatchDataType("unitAbilityData", typeof(UnitAbility.Type)); //...casual...      ...fapingvin carry...       ...again
